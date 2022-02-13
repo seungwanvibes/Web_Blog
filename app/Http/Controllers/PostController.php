b177829b7cbb2f39af79748ec3023046806c1957
DESKTOP-HN0EI4G\Nabila <nabilahapriliasalsabila@gmail.com>
2022-02-13 19:44:42 +0700

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -25,7 +25,7 @@ class PostController extends Controller
         return view('posts', [
             "title" => "All Posts" . $title,
             "active" => 'posts',
-            "posts" => Post::latest()->with((['search', 'category', 'author']))->paginate(7)->withQueryString()
+            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
         ]);
     }
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -27,14 +27,13 @@
         <div class="card mb-3">  
         @if ($posts[0]->image)
             <div style="max-height : 400px; overflow:hidden;">
-            <img src="{{Storage :: url ($posts[0]-> image)}}" alt="{{ $posts[0]->category->name }}" class="img-fluid">
+            <img src="{{Storage :: url ($post-> image)}}" alt="{{ $posts[0]->category->name }}" class="img-fluid">
             </div>
         @else
         <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">    
         @endif
        
             <div class="card-body text-center">
-                
             <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
             <p>
                 <small class="text-muted">
@@ -57,7 +56,7 @@
                     <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.5)"><a href="/blog?category={{ $post->category->slug }}" class="text-white text-decoration-none">{{  $post->category->name }}</a></div>
 
                     @if ($post->image)
-                    <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid">
+                    <img src="{{Storage :: url ($post-> image)}}" alt="{{ $post->category->name }}" class="img-fluid">
                     @else
                     <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">    
                     @endif
